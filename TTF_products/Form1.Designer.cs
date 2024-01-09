5479f5e469c40ebe6a66f7ab8e501236a038feaa
JordanRobo <jordan@stateot.art>
2024-01-10 01:07:35 +1100

    Add project files.

---
+++ TTF_products/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TTF_products
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openfile = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openfile
+            // 
+            this.openfile.Location = new System.Drawing.Point(370, 514);
+            this.openfile.Name = "openfile";
+            this.openfile.Size = new System.Drawing.Size(284, 57);
+            this.openfile.TabIndex = 0;
+            this.openfile.Text = "Import .csv File";
+            this.openfile.UseVisualStyleBackColor = true;
+            this.openfile.Click += new System.EventHandler(this.openfile_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(34, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 72;
+            this.dataGridView1.RowTemplate.Height = 31;
+            this.dataGridView1.Size = new System.Drawing.Size(965, 434);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1030, 723);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.openfile);
+            this.Name = "Form1";
+            this.Text = "Awesomeness Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openfile;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ TTF_products/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TTF_products
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+       private void openfile_Click(object sender, EventArgs e)
+        {
+       
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // create a new datatable
+                DataTable dt = new DataTable();
+                // create a new data row
+                DataRow dr;
+                // create a new data column
+                DataColumn dc;
+                // create a new string array
+                string[] lines = System.IO.File.ReadAllLines(openFileDialog1.FileName);
+                // create a new string array
+                string[] dataCol = null;
+                // create a new int variable
+                int x = 0;
+
+                // loop through the lines array
+                foreach (string line in lines)
+                {
+                    // split the line at the commas
+                    dataCol = line.Split(',');
+                    // if x is 0
+                    if (x == 0)
+                    {
+                        // loop through the dataCol array
+                        for (int i = 0; i <= dataCol.Count() - 1; i++)
+                        {
+                            // create a new data column
+                            dc = new DataColumn(dataCol[i].ToString());
+                            // add the data column to the datatable
+                            dt.Columns.Add(dc);
+                        }
+                        // increment x
+                        x++;
+                    }
+                    else
+                    {
+                        // create a new data row
+                        dr = dt.NewRow();
+                        // loop through the dataCol array
+                        for (int i = 0; i <= dataCol.Count() - 1; i++)
+                        {
+                            // add the dataCol to the data row
+                            dr[i] = dataCol[i].ToString();
+                        }
+                        // add the data row to the datatable
+                        dt.Rows.Add(dr);
+                    }
+                }
+                // set the data source of the dataGridView1 to the datatable
+                dataGridView1.DataSource = dt;
+            }
+
+
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
